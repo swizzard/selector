627bf18e6a1771aff1bf9c2426a21a7ac1ee80ae
Sam Raker <sraker@elitedaily.com>
2015-07-06 09:09:29 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Install selector
+"""
+
+from setuptools import setup
+
+
+setup(
+        name='selector',
+        version='0.1.0',
+        description="'select'-type behavior over generators",
+        url='https://github.com/swizzard/selector',
+        author='Sam Raker',
+        author_email='sam.raker@gmail.com',
+        license='WTFPL',
+        py_modules=['selector'],
+        classifiers=['Developemt Status :: 4 - Beta',
+                     'Programming Language :: Python :: 2.7']
+        )
